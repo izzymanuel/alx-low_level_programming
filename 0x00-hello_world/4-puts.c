d7b85dc8fe58c5ed66a5ce64aa1001808eefc464
Izuchukwu Emmanuel <izzymanuel@gmail.com>
2022-07-01 13:44:32 -0700

    puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-/
- * main - Entry point
- *  * Return: Always 0 (sucess)
- *   */
- int main(void)
+/**
+ * main - main block
+ * Return: 0
+ */
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
