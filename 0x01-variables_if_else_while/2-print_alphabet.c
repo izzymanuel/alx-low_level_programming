684596cb5a4366dedcd70c5fc6833ca5f082237d
Izuchukwu Emmanuel <izzymanuel@gmail.com>
2022-07-06 11:27:29 -0700

    done

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabet in lowercase
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char x = 'a';
+
+	while (x != '{')
+	{  putchar(x);
+		x++;
+	}
+	putchar('\n');
+	return (0);
+}
