a69f597b207e436b711c009fb47e9499c64a6d62
Izuchukwu Emmanuel <izzymanuel@gmail.com>
2022-07-07 22:20:46 -0700

    more functions

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isdigit - Write a function that checks for a digit (0 through 9).
+ * @c: digit to be checked
+ * Return: int depending on input
+ */
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	else
+		return (0);
+}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - entry point
+ * @a: first varable to he multiplied
+ * @b: second variable to be multiplied
+ * Return: int
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
